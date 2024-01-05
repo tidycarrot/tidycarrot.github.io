615488caf0d886b85b4f5845704026ad2e6ec27b
tidycarrot <tidy.carrot@gmail.com>
2024-01-05 16:22:01 +1100

    Add drop shadow to button

--- sus.js
+++ sus.js
@@ -4,29 +4,21 @@ function rickroll(){
 
 function move(){
   
-  
     //the script
-      var button = document.getElementById('lol');
-      //todo: mc hammer audio trigger
+    var button = document.getElementById('lol');
+
+    button.style.position = 'absolute';
+    button.style.left = '0px';
+    button.style.top = '0px';
   
-  
-      button.style.position = 'absolute';
-      button.style.left = '0px';
-      button.style.top = '0px';
-  
-      button.addEventListener('mouseover', function(event) {
-          var buttonWidth = button.offsetWidth;
-          var buttonHeight = button.offsetHeight;
-  
-          var windowWidth = window.innerWidth;
-          var windowHeight = window.innerHeight;
-  
-          var randomX = Math.random() * (windowWidth - buttonWidth);
-          var randomY = Math.random() * (windowHeight - buttonHeight);
-  
-          button.style.left = randomX + 'px';
-          button.style.top = randomY + 'px';
-      });
+    var buttonWidth = button.offsetWidth;
+    var buttonHeight = button.offsetHeight;
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var randomX = Math.random() * (windowWidth - buttonWidth);
+    var randomY = Math.random() * (windowHeight - buttonHeight);
+    button.style.left = randomX + 'px';
+    button.style.top = randomY + 'px';    
 }
 
 /*
@@ -46,28 +38,6 @@ function resetLolBtn() {
     $(this).css({
         position: "static"
     });
-}
-
-
-lolBtn.on("mouseover", function() {
-    console.log("whoops");
-    count += 1;
-    console.log(count);
-
-    resetLolBtn();
-
-    $(this).css({
-        position: "absolute",
-        top: randNum(90) + "%",
-        left: randNum(90) + "%"
-    });
-});
-lolBtn.on("click", function() {
-    alert("Either there was a glitch in the matrix or you are one sly devil. It took you " + count + " attempts.")
-    alert("Don't forget to comment your number of attempts. I hope you enjoyed this pen. =)");
-    modal.fadeOut(150);
-});
-
 }
 
 */

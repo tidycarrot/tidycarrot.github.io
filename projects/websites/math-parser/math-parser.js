6a639e23db2d7355306e6ae832df742187645bbe
tidycarrot <tidy.carrot@gmail.com>
2024-01-17 21:36:19 +1100

    Add tokenizer and notes file. Make cahnges to date in projects page.

--- projects/websites/math-parser/math-parser.js
+++ projects/websites/math-parser/math-parser.js
@@ -22,8 +22,78 @@ function isWhitespace(c) {
         || c === '\ufeff'
 }
 
-const op_precedence = {
-    '+': 1,
+// Returns next non-whitespace after array[current], or null if none
+function nextCharacter(array, current) {
+    for (let i = current; i < array.length; i++) {
+        if (isWhitespace(array[i]))
+            continue;
+        return array[i];
+    }
+    return null;
+}
+
+const operators = {
+    '+': {
+        precedence: 1,
+        associativity: "left",
+        type: "binary",
+    },
+    '-': {
+        precedence: 1,
+        associativity: "left",
+        type: "binary",
+    },
+    '/': {
+        precedence: 2,
+        associativity: "left",
+        type: "binary",
+    },
+    '*': {
+        precedence: 2,
+        associativity: "left",
+        type: "binary",
+    },/*
+    '%':{
+        precendence: 2,
+        associativity: "left",
+        type: "binary",
+    },
+    '^':{
+        precendence: 3,
+        associativity: "right",
+        type: "binary",
+    },
+    '!':{
+        precendence: 4,
+        associativity: "left",
+        type: "unary",
+    },
+    'sin':{
+        precendence: 4,
+        associativity: "right",
+        type: "unary",
+        function: Math.sin,
+    },
+    'cos':{
+        precendence: 4,
+        associativity: "right",
+        type: "unary",
+        function: Math.cos,
+    },
+    'tan':{
+        precendence: 4,
+        associativity: "right",
+        type: "unary",
+        function: Math.tan,
+    },
+    'abs':{
+        precedence: 4,
+        associativity: "right",
+        type: "unary",
+        function: Math.abs,
+    }
+    */
+
 
 };
 
@@ -32,30 +102,34 @@ function tokenize(equation) {
     let decimal = false;
     let num;
 
-    for (let i = 0; i < equation.length; i++) {
+    for (let i = 0; i <= equation.length; i++) {
 
         let curr = equation[i];
         if (isWhitespace(curr))
             continue;
-        let next;
-
-        if (curr < equation.length) next = equation[i + 1];
-        else next = null;
+        let next = nextCharacter(equation, i);
 
         switch (curr) {
-            case '^':
+
             case '+':
             case '-':
             case '/':
             case '*':
-            case '%':
-            case '!':
             case ')':
                 tokens.push({ type: 'op', data: curr });
                 decimal = false;
                 break;
             case '(':
-                if (!isNaN(equation[i]))
+                if (!isNaNtokens[-1]) {
+                    tokens.push({ type: 'num', data: curr });
+                    decimal = false;
+                    break;
+                } else {
+                    tokens.push({ type: 'op', data: curr });
+                    decimal = false;
+                    break;
+                }
+                break;
             default:
                 if (!isNaN(curr)) {
                     num += curr;

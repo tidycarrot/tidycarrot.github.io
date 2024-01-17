01e898faf6490f49b3cafac90799cb1f4e7a3e2b
tidycarrot <tidy.carrot@gmail.com>
2024-01-17 17:45:59 +1100

    Co-authored-by: Bayemite. Improve project page css, add math parser

--- css-showcase.js
+++
@@ -1,9 +0,0 @@
-function changeImage() {
-    document.getElementById("customImage").style.filter = 
-    'blur(' + document.querySelector('#blurImage').value +'px)' + 
-    'brightness(' + document.querySelector('#brightImage').value + '%)' +
-    'contrast(' + document.querySelector('#contrastImage').value + '%)' +
-    'grayscale(' + document.querySelector('#grayscaleImage').value + '%)' +
-    '';
-    document.getElementById("blurAmount").innerHTML = document.querySelector('#blurImage').value + "px";
-}
